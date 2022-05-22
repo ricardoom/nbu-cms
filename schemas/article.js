ea1bcddd7e61689b712f211b72d11cd787c47731
ricardoom <ricardo.e.galvez@gmail.com>
2022-05-22 12:26:30 -0700

    refactor: commiting to find out where the home page went...

---
+++ schemas/article.js
@@ -0,0 +1,30 @@
+// ./studio/schemas/article.js
+
+export default {
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    {
+      name: 'title', 
+      type: 'string'
+    },
+    // Important! Document needs a slug for Next.js to query for.
+    {
+      name: 'slug',
+      type: 'slug',
+      options: 
+        {source: 'title'
+      }
+    },
+    {
+      name: 'content', 
+      type: 'array',
+      of: [
+        {
+          type : 'block'
+        }
+      ],
+    },
+  ],
+}
