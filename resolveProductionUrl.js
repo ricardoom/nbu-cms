88cf4074b8543bd2965431343ce0a26f59bfe8a9
ricardoom <ricardo.e.galvez@gmail.com>
2022-05-25 16:28:47 -0700

    fix: :zap: URL fix up for previews

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -2,5 +2,5 @@
 // TODO: make sure this gets set up correctly for dev and production environments
 export default function resolveProductionUrl(document) {
   // return `https://nativeboundunbound/posts/${document.slug.current}`
-  return `http://localhost:7890/${document.slug.current}`
+  return `http://localhost:7890/posts/${document.slug.current}`
 }
