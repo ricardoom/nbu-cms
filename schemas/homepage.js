3d3224c5acd9aa04eb88b003046440604781b3c6
ricardoom <ricardo.e.galvez@gmail.com>
2022-06-16 12:41:12 -0700

    refactor(global): :art: Removed commetned out code

--- schemas/homepage.js
+++ schemas/homepage.js
@@ -3,18 +3,6 @@ export default {
   title: 'Homepage',
   type: 'document',
   fields: [
-    // {
-    //   name: 'title',
-    //   title: 'Homepage title',
-    //   type: 'string',
-    //   description: "What's the title website?",
-    // },
-    // {
-    //   name: 'subtitle',
-    //   title: 'Homepage subtitle',
-    //   type: 'string',
-    //   description: "What's the subtitle of the homepage hero?",
-    // },
     {
       name: 'body',
       title: 'Homepage Body',
